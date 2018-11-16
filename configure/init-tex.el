78ac5d52383be829e2c778924dd751101042bb90
zhou_c <zhou_c@hoperun.com>
2018-11-16 18:22:15 +0800

    Update the AucTeX configuration
    Just update the AucTeX configuration.
    
    Signed-off-by: zhou_c <zhou_c@hoperun.com>

--- configure/init-tex.el
+++ configure/init-tex.el
@@ -1,14 +1,16 @@
 (setq TeX-command-extra-options "-shell-escape")
 (setq TeX-engine (quote xetex))
 
-(setq TeX-auto-save       t)
 (setq TeX-clean-confirm   nil)
+
+(setq TeX-auto-save       t)
+(setq TeX-parse-self      t)
+(setq-default TeX-master  nil)
 
 (setq TeX-electric-escape t)
 (setq TeX-fold-auto       t)
 (setq TeX-fold-mode       t)
 (setq TeX-fold-type-list  (quote (env math)))
-(setq TeX-parse-self      t)
 ;; (setq TeX-shell            "/bin/bash")
 
 (setq LaTeX-fold-env-spec-list
@@ -21,8 +23,5 @@
          ("minted"))
         ("[Item List]"
          ("itemize")))))
-(setq LaTeX-mode-hook     (quote (preview-mode-setup turn-on-reftex)))
-
-(setq reftex-plug-into-AUCTeX t)
 
 (provide 'init-tex)
